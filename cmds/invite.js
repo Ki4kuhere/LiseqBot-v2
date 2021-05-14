e92ef84c1272ecc7c5355f4b5f5012070ab7a3f8
PolskiLis19 <63364127+PolskiLis19@users.noreply.github.com>
2021-05-14 20:41:35 +0200

    invite bugfix

--- cmds/invite.js
+++ cmds/invite.js
@@ -1,7 +1,7 @@
 const discord = require('discord.js');
 
 module.exports.run = (bot , message, args, prefix) => {
-    message.channel.send("https://discord.com/api/oauth2/authorize?client_id=834849629254058036&permissions=0&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fdashboard%2Fcallback&scope=bot");
+    message.channel.send("https://discord.com/api/oauth2/authorize?client_id=834849629254058036&permissions=0&redirect_uri=https%3A%2F%2Fliseqbot.pl&scope=bot");
 };
 module.exports.help = {
     name: "invite",
